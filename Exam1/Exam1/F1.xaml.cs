1f29880f087610a6c8b5f148ae9489e74079f1a2
Asya-Razumova <130699267+Asya-Razumova@users.noreply.github.com>
2023-04-16 12:32:54 +0300

    Add files via upload

---
+++ Exam1/Exam1/F1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam1
+{
+    /// <summary>
+    /// Логика взаимодействия для F1.xaml
+    /// </summary>
+    public partial class F1 : Page
+    {
+        Exam1Entities ex = new Exam1Entities();
+        public F1()
+        {
+            InitializeComponent();
+            Zav.ItemsSource = ex.Zayavky.ToList();
+        }
+    }
+}
---
+++ Exam1/Exam1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Exam1Entities ex = new Exam1Entities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Vxod_Click(object sender, RoutedEventArgs e)
+        {
+            string log1 = login12.Text.ToString();
+            var zapr = ex.Sotrudnik.Single(s => s.Login == log1);
+            string l1 = zapr.ToString();
+            string p1 = zapr.ToString();
+
+            if (login12.Text != "" && Parol.Password != "")
+            {
+
+                if (login12.Text != "")
+                {
+                    Perexod.Navigate(new F1());
+                }
+
+            }
+            else { MessageBox.Show("Данные введены неверно!"); }
+        }
+    }
+}
